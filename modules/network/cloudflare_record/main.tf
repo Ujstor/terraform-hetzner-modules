9b6cf12fbbe9fd45b48129d8b7d04fb73656ace4
Ujstor <astipan@gmail.com>
2024-12-22 02:57:27 +0100

    add cloudflare priority

--- modules/network/cloudflare_record/main.tf
+++ modules/network/cloudflare_record/main.tf
@@ -1,10 +1,11 @@
 resource "cloudflare_record" "domain_recorda" {
   for_each = var.cloudflare_record
 
-  zone_id = each.value.zone_id
-  name    = each.value.name
-  content = each.value.content
-  type    = each.value.type
-  ttl     = each.value.ttl
-  proxied = each.value.proxied
+  zone_id  = each.value.zone_id
+  name     = each.value.name
+  content  = each.value.content
+  type     = each.value.type
+  ttl      = each.value.ttl
+  proxied  = each.value.proxied
+  priority = each.value.priority
 }
--- modules/network/cloudflare_record/variables.tf
+++ modules/network/cloudflare_record/variables.tf
@@ -1,11 +1,12 @@
 variable "cloudflare_record" {
   description = "Cloudflare record configuration"
   type = map(object({
-    zone_id = string
-    name    = string
-    content = string
-    type    = string
-    ttl     = optional(number)
-    proxied = optional(bool)
+    zone_id  = string
+    name     = string
+    content  = string
+    type     = string
+    ttl      = optional(number)
+    proxied  = optional(bool)
+    priority = optional(number)
   }))
 }
